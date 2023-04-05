543c6ae5157457a2baaa146019f9ec7376073a4e
Anniel <anniel.marrero@agence.cl>
2023-04-04 21:59:01 -0400

    new chaneges

--- WebApplication10/Controllers/WeatherForecastController.cs
+++ WebApplication10/Controllers/WeatherForecastController.cs
@@ -30,18 +30,6 @@ namespace WebApplication10.Controllers
         }
 		
 		
-		[HttpGet(Name = "fromGitHub1")]
-        public IEnumerable<WeatherForecast> fromGitHub1()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-		
 		
 
         [HttpGet(Name = "GetWeatherForecast")]
