c6f2a1e7d69bfbcfc6fd81fcfe1c702bd8dab482
Anniel <anniel.marrero@agence.cl>
2023-04-04 21:40:24 -0400

    added local changes

--- WebApplication10/Controllers/WeatherForecastController.cs
+++ WebApplication10/Controllers/WeatherForecastController.cs
@@ -29,5 +29,41 @@ namespace WebApplication10.Controllers
             })
             .ToArray();
         }
+        [HttpGet(Name = "fromLocal")]
+        public IEnumerable<WeatherForecast> fromLocal()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet(Name = "fromLocal1")]
+        public IEnumerable<WeatherForecast> fromLocal1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "fromLocal2")]
+        public IEnumerable<WeatherForecast> fromLocal2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
